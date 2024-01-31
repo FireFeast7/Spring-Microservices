3f3bf45f8e36afd4e761a34474c9e848c97ea400
Ameya <ameyasurana10@gmail.com>
2024-02-01 00:23:55 +0530

    Initial Commit

---
+++ src/main/java/org/backend/rabbitmq/controller/RabbitMQController.java
@@ -0,0 +1,34 @@
+package org.backend.rabbitmq.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.backend.rabbitmq.dto.UserDto;
+import org.backend.rabbitmq.publisher.RabbitMQJsonProducer;
+import org.backend.rabbitmq.publisher.RabbitMQProducer;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(path = "/api/v1")
+@RequiredArgsConstructor
+public class RabbitMQController {
+
+    private final RabbitMQProducer rabbitMQProducer;
+    private final RabbitMQJsonProducer rabbitMQJsonProducer;
+
+    @GetMapping(path = "/publish")
+    public ResponseEntity<String> sendMessage(
+            @RequestParam("message") String message
+    ){
+        rabbitMQProducer.sendMessage(message);
+        return ResponseEntity.ok("Message Sent");
+    }
+
+    @PostMapping(path = "/publishJson")
+    public ResponseEntity<String> sendJsonMessage(
+            @RequestBody UserDto userDto
+            ){
+        rabbitMQJsonProducer.sendJsonMessage(userDto);
+        return ResponseEntity.ok("Json Message Sent");
+    }
+
+}
---
+++ src/main/java/org/backend/rabbitmq/dto/UserDto.java
@@ -0,0 +1,14 @@
+package org.backend.rabbitmq.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDto {
+    private int id;
+    private String firstName;
+    private String lastName;
+}
